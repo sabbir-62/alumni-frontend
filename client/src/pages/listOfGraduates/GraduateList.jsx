cbcab4c40cadd6130eb014f2f89049967c4551e6
sabbir-62 <sabbir.hossain.se62@gmail.com>
2023-10-21 02:47:47 +0600

    [In progress]

--- client/src/pages/listOfGraduates/GraduateList.jsx
+++ client/src/pages/listOfGraduates/GraduateList.jsx
@@ -1,113 +1,86 @@
+
 import { useEffect, useState } from 'react';
 import { useNavigate  } from 'react-router-dom';
 import Cookies from "js-cookie";
 import './graduateList.css'
-
 import { toast } from 'react-toastify';
 import { BeatLoader } from "react-spinners";
 
 const GraduateList = () => {
     const [loading, setLoading] = useState(true);
-    const [state, setState] = useState({
-        name: [],
-        department: [],
-        passingYear: [],
-        email: [],
-        phone: [],
-        company: [],
-        role: []
-    });
+    const [graduates, setGraduates] = useState([]);
 
-    const navigate = useNavigate(); // Call useNavigate as a function
+    const navigate = useNavigate();
 
     const list = async() => {
         const url = "http://localhost:8000/api/v1/graduates-list";
         const cookie = Cookies.get("myCookie");
 
-        if(!cookie){
-            toast.warning("Please Login")
-            navigate('/login')
+        if (!cookie) {
+            toast.warning("Please Login");
+            navigate('/login');
         }
 
-        await fetch(url, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-        .then((response) => response.json())
-        .then((data) => {
+        try {
+            const response = await fetch(url, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+            const data = await response.json();
+
             if (!data.success) {
                 toast.error(data.message);
+            } else {
+                setGraduates(data.data);
+                setLoading(false);
             }
-            else {
-                // Extract titles, descriptions, and dates into separate arrays
-                const names = data.data.map(item => item.name);
-                const departments = data.data.map(item => item.department);
-                const passingYears = data.data.map(item => item.passingYear);
-                const emails = data.data.map(item => item.email);
-                const phones = data.data.map(item => item.phone);
-                const companies = data.data.map(item => item.company);
-                const roles = data.data.map(item => item.role);
-
-                
-                // Update the state with the extracted data
-                setState({
-                    name: names,
-                    department: departments,
-                    passingYear: passingYears,
-                    email: emails,
-                    phone: phones,
-                    company: companies,
-                    role: roles
-                });
-                setLoading(false)
-                 
-            }
-        });
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
     }
-
-    console.log("name: ", state.name);
-
 
     useEffect(() => {
         list();
     }, []);
 
-
-
     return (
-        <div>
-          {
-            loading ?
-            (
+        <div className='min-height'>
+            {loading ? (
                 <div className="loader">
-                  <BeatLoader
-                    color={"#36d7b7"}
-                    loading={loading}
-                    size={15}
-                  />
+                    <BeatLoader color={"#36d7b7"} loading={loading} size={15} />
                 </div>
-              )
-            :
-            <div className="container lists">
-            <div className="row list">
-                {state.name.map((name, index) => 
-                    ( 
-                    <div key={index} className="m-3 list-block p-5">
-                        <p className="name">Name: {name}</p>
-                        <p className="department">Department: {state.department[index]}</p>
-                        <p className="passingYear">Passing Year: {state.passingYear[index]}</p>
-                        <p className="email">Email: {state.email[index]}</p>
-                        <p className="phone">Phone: {state.phone[index]}</p>
-                        <p className="company">Current Company: {state.company[index]}</p>
-                        <p className="role">Role: {state.role[index]}</p>
-                    </div>
-                ))}
-                   
-                </div>    
-            </div>
-          }
+            ) : (
+                <div className="container lists">
+                    <table className="table list-table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Department</th>
+                                <th>Passing Year</th>
+                                <th>Current Company</th>
+                                <th>Profession</th>
+                                <th>Email</th>
+                                <th>Phone</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {graduates.map((graduate, index) => (
+                                <tr key={index}>
+                                    <td>{graduate.name}</td>
+                                    <td>{graduate.department}</td>
+                                    <td>{graduate.passingYear}</td>
+                                    <td>{graduate.company}</td>
+                                    <td>{graduate.role}</td>
+                                    <td>{graduate.email}</td>
+                                    <td>{graduate.phone}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            )}
         </div>
     );
 };
